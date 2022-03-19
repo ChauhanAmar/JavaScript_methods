498279db658155762cd9898447e2d6ebd2fca795
Amar Chauhan <89099211+ChauhanAmar@users.noreply.github.com>
2022-03-19 22:20:01 +0530

    Add files via upload

---
+++ JavaScript/javascript.js
@@ -0,0 +1,44 @@
+// //Leap year
+// var year=2100;
+// debugger;
+// if(year%4===0){
+//     if(year%100===0){
+//         if(year%400===0){
+//             console.log('leap year');
+//         }
+//         else{
+//             console.log('No leap year');
+//         }}else
+//       {  
+//      console.log('leap year');
+//       }}
+//      else {
+//          console.log('No leap year');
+//      }
+// //while loop
+// var num =0;
+// while(num<=10){
+//     console.log(num);
+//     num++;
+// }
+    
+
+//Do while loop
+// var num =0;
+// do{
+//     console.log(num);
+//     num++;
+// }while(num<=10)
+
+// const months=['Jan','march','April', 'June','July'];
+// const indexOfMonth = months.indexOf('June');
+// if(indexOfMonth != -1){
+//     const deleteMonth=months.splice(indexOfMonth,1);
+//     console.log(months);
+// }else{
+//     console.log("No such data found");
+// }
+
+
+
+
